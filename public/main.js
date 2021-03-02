72b2cbb30362d94a704a1f3273905e4d101c09ec
Chayton Hamric <ChaytonHamric@gmail.com>
2021-03-02 14:27:41 -0500

    Made 3D spinning cube with diff side colors

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 
-var squareRotation = 0.0;
+var cubeRotation = 0.0;
 
 main();
 
@@ -131,33 +131,108 @@ function loadShader(gl, type, source) {
 function initBuffer(gl){
 
     // Shape Array
+    // const positions = [
+    //     1.0, 1.0,
+    //     -1.0, 1.0,
+    //     1.0, -1.0,
+    //     -1.0, -1.0,
+    // ];
+
     const positions = [
-        1.0, 1.0,
-        -1.0, 1.0,
-        1.0, -1.0,
-        -1.0, -1.0,
+        // Front face
+        -1.0, -1.0, 1.0,
+        1.0, -1.0, 1.0,
+        1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+
+        // Back face
+        -1.0, -1.0, -1.0,
+        -1.0, 1.0, -1.0,
+        1.0, 1.0, -1.0,
+        1.0, -1.0, -1.0,
+
+        // Top face
+        -1.0, 1.0, -1.0,
+        -1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+        1.0, 1.0, -1.0,
+
+        // Bottom face
+        -1.0, -1.0, -1.0,
+        1.0, -1.0, -1.0,
+        1.0, -1.0, 1.0,
+        -1.0, -1.0, 1.0,
+
+        // Right face
+        1.0, -1.0, -1.0,
+        1.0, 1.0, -1.0,
+        1.0, 1.0, 1.0,
+        1.0, -1.0, 1.0,
+
+        // Left face
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0, 1.0,
+        -1.0, 1.0, 1.0,
+        -1.0, 1.0, -1.0,
     ];
 
-    //color Array
-    const colors = [
-        1.0, 0.0, 1.0, 1.0, // (Top Right)
-        1.0, 0.0, 1.0, 1.0, // (Top Left)
-        0.0, 0.0, 1.0, 1.0, // (Bottom Right)
-        0.0, 0.0, 1.0, 1.0, // (Bottom Left)
-    ]
+    // 2D Color Array
+    // const colors = [
+    //     1.0, 0.0, 1.0, 1.0, // (Top Right)
+    //     1.0, 0.0, 1.0, 1.0, // (Top Left)
+    //     0.0, 0.0, 1.0, 1.0, // (Bottom Right)
+    //     0.0, 0.0, 1.0, 1.0, // (Bottom Left)
+    //]
+
+    const faceColors = [
+        [1.0, 1.0, 1.0, 1.0],    // Front face: white
+        [1.0, 0.0, 0.0, 1.0],    // Back face: red
+        [0.0, 1.0, 0.0, 1.0],    // Top face: green
+        [0.0, 0.0, 1.0, 1.0],    // Bottom face: blue
+        [1.0, 1.0, 0.0, 1.0],    // Right face: yellow
+        [1.0, 0.0, 1.0, 1.0],    // Left face: purple
+    ];
+
     // Create Position Buffer
     const positionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
     // Create Color Buffer
+    var colors = [];
+    for (var j = 0; j < faceColors.length; ++j) {
+        const c = faceColors[j];
+
+        // Repeat each color four times for the four vertices of the face
+        colors = colors.concat(c, c, c, c);
+    }
     const colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
+    const indices = [
+        0, 1, 2,     0, 2, 3,    // front
+        4, 5, 6,     4, 6, 7,    // back
+        8, 9, 10,    8, 10, 11,   // top
+        12, 13, 14,  12, 14, 15,   // bottom
+        16, 17, 18,  16, 18, 19,   // right
+        20, 21, 22,  20, 22, 23,   // left
+    ];
+
+    // Now send the element array to GL
+
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
+        new Uint16Array(indices), gl.STATIC_DRAW);
+
+
+
     return {
         position: positionBuffer, 
         color: colorBuffer,
+        indices: indexBuffer,
     };
 }
 
@@ -188,13 +263,14 @@ function drawScene(gl, ProgramInfo, buffers, deltaTime){
     const modelViewMatrix = mat4.create();
     mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
 
-    squareRotation += deltaTime;
+    cubeRotation += deltaTime;
 
-    mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation, [0, 0, 1]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation, [0, 0, 1]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, cubeRotation, [0, 1, 0]);
 
     // Pulling position to webGL
     {
-        const numComponents = 2;
+        const numComponents = 3;
         const type = gl.FLOAT;
         const normalize = false;
         const stride = 0;
@@ -230,12 +306,21 @@ function drawScene(gl, ProgramInfo, buffers, deltaTime){
             ProgramInfo.attribLocations.vertexColor);
     }
 
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
+
     // Use Program when drawing
     gl.useProgram(ProgramInfo.program);
 
     // Set shader uniforms
     gl.uniformMatrix4fv(ProgramInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
     gl.uniformMatrix4fv(ProgramInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+
+    {
+        const vertexCount = 36;
+        const type = gl.UNSIGNED_SHORT;
+        const offset = 0;
+        gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    }
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     
