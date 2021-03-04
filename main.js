a65cb6e9192560fba7490ac04c98dbac81738015
Chayton Hamric <ChaytonHamric@gmail.com>
2021-03-04 14:13:23 -0500

    clean up

--- main.js
+++ main.js
@@ -101,22 +101,22 @@ function main() {
     
     // Coloring of 3D Model
     const faceColors = [
-        [0.0, 0.0, 0.0, 1.0],    // Front face: white
-        [1.0, 0.0, 0.0, 1.0],    // Back face: red
-        [0.0, 1.0, 0.0, 1.0],    // Top face: green
-        [0.0, 0.0, 1.0, 1.0],    // Bottom face: blue
-        [1.0, 1.0, 0.0, 1.0],    // Right face: yellow
-        [1.0, 0.0, 1.0, 1.0],    // Left face: purple
+        [0.0, 0.0, 0.0, 1.0],    // Front face
+        [1.0, 0.0, 0.0, 1.0],    // Back face
+        [0.0, 1.0, 0.0, 1.0],    // Top face
+        [0.0, 0.0, 1.0, 1.0],    // Bottom face
+        [1.0, 1.0, 0.0, 1.0],    // Right face
+        [1.0, 0.0, 1.0, 1.0],    // Left face
     ];
 
     // Indices for individual vectors
     const indices = [
-        0, 1, 2, 0, 2, 3,    // front
-        4, 5, 6, 4, 6, 7,    // back
-        8, 9, 10, 8, 10, 11,   // top
-        12, 13, 14, 12, 14, 15,   // bottom
-        16, 17, 18, 16, 18, 19,   // right
-        20, 21, 22, 20, 22, 23,   // left
+        0, 1, 2, 0, 2, 3,           // front
+        4, 5, 6, 4, 6, 7,           // back
+        8, 9, 10, 8, 10, 11,        // top
+        12, 13, 14, 12, 14, 15,     // bottom
+        16, 17, 18, 16, 18, 19,     // right
+        20, 21, 22, 20, 22, 23,     // left
     ];
 
     // Rotation and Transition data
